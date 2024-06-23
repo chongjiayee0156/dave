2c03025d64097ef605d4330f472b6c4d2312afde
jcho0156 <jcho0156@student.monash.edu>
2024-06-23 23:21:52 +0800

    deploy

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://localhost:3200',
+    baseUrl: 'https://swiftnote.render.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
